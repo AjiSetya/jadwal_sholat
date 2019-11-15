ac5cee3b3c1d4e325586d8231e09d02d19a5883e
AjiSetya <setyaaji07@gmail.com>
2019-11-15 15:13:52 +0700

    add class data

---
+++ lib/model/today_weather.dart
@@ -0,0 +1,15 @@
+class Today_weather {
+  int pressure;
+  String temperature;
+
+  Today_weather.fromJsonMap(Map<String, dynamic> map)
+      : pressure = map["pressure"],
+        temperature = map["temperature"];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['pressure'] = pressure;
+    data['temperature'] = temperature;
+    return data;
+  }
+}
